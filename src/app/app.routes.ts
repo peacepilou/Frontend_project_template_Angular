b67b70a9f61d4a532198ab4558d1248bbb07af39
pilou <p-l.b@hotmail.com>
2024-12-04 13:41:42 +0100

    ignored prettier cypress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {
-        path: '', component: AppComponent
-    }
+  {
+    path: '',
+    component: AppComponent,
+  },
 ];
