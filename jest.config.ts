19c681c43271a53dcc629444b11fa158ba1cceb6
pilou <p-l.b@hotmail.com>
2025-01-10 17:35:24 +0100

    updated test

--- jest.config.ts
+++ jest.config.ts
@@ -8,10 +8,10 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
   clearMocks: true,
   coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: 'node',
+  coverageProvider: "v8"
 };
 
 export default config;
--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,14 +1,11 @@
 import { verifyEnvironment } from './environments/verify-environment';
-import * as main from './main';
 
-// jest.mock('./environments/verify-environment', () => ({
-//     verifyEnvironment: jest.fn(),
-// }));
+jest.mock('./environments/verify-environment', () => ({
+    verifyEnvironment: jest.fn(),
+}));
 
 describe('main.ts', () => {
     it('should call verifyEnvironment', () => {
-        // expect(verifyEnvironment).toBeDefined();
-        expect(1).toBe(1)
-            ;
+        expect(verifyEnvironment).toBeDefined();
     });
 });
