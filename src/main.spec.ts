170856344a89350bedd32763f2e07e35e867c118
pilou <p-l.b@hotmail.com>
2025-01-10 17:23:47 +0100

    updated test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,13 +1,14 @@
 import { verifyEnvironment } from './environments/verify-environment';
 import * as main from './main';
 
-jest.mock('./environments/verify-environment', () => ({
-    verifyEnvironment: jest.fn(),
-}));
+// jest.mock('./environments/verify-environment', () => ({
+//     verifyEnvironment: jest.fn(),
+// }));
 
 describe('main.ts', () => {
     it('should call verifyEnvironment', () => {
         // expect(verifyEnvironment).toBeDefined();
-        expect(1).toBe(1);
+        expect(1).toBe(1)
+            ;
     });
 });
